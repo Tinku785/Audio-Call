a3520c44ab76ad1820072d9733ba5acc6c40edf5
Tinku moni kaushik <tinkukaushik783@gmail.com>
2026-01-03 16:48:40 +0530

    Implement message storage and history retrieval
    Added memory storage for chat messages and implemented message history retrieval for new users joining a room.

--- server.js
+++ server.js
@@ -8,24 +8,42 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
+// --- 1. THE MEMORY STORAGE ---
+// This object will hold all messages: { 'room-1': [msg1, msg2], 'room-2': [] }
+const roomMessages = {}; 
+
 io.on('connection', (socket) => {
     console.log('User connected: ' + socket.id);
 
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
         socket.to(roomId).emit('user-connected', userId);
+
+        // --- 2. SEND HISTORY TO NEW USER ---
+        // If this room has history, send it to the person who just joined
+        if (roomMessages[roomId]) {
+            socket.emit('load-messages', roomMessages[roomId]);
+        }
     });
 
-    // --- THIS WAS MISSING OR NOT WORKING ---
-  // UPDATED: Handle Text Chat with Names
     socket.on('send-message', (data) => {
-        // We now send an object: { text: "Hello", user: "Tinku" }
+        // --- 3. SAVE THE MESSAGE ---
+        if (!roomMessages[data.roomId]) {
+            roomMessages[data.roomId] = []; // Create room list if not exists
+        }
+        
+        // Add message to the list
+        roomMessages[data.roomId].push({
+            sender: data.senderName,
+            message: data.message
+        });
+
+        // Broadcast to others
         socket.to(data.roomId).emit('receive-message', {
             message: data.message, 
-            sender: data.senderName  // <--- NEW: Sending the name
+            sender: data.senderName
         });
     });
-    // ---------------------------------------
 
     socket.on('signal', (data) => {
         socket.to(data.roomId).emit('signal', {
